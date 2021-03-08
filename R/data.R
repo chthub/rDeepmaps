06f22fb1bc4e345988d71ae885ccc141bb59bb7e
Cankun Wang <flykun0620@gmail.com>
2021-03-08 12:47:44 -0500

    Update example data description

--- R/data.R
+++ R/data.R
@@ -2,11 +2,12 @@
 #'
 #' Yan 2013: Human embryo, 7 cell types, 90 cells
 #'
-#' @format A list with expr and labels:
-#' $expr: a data frame with 20214 genes (rows) amd 90 cells (columns)
+#' @format A list with expression matrix and metadata:
+#'
 #' $meta:
 #' \describe{
-#'   \item{Cluster}{fct cell type}
+#'   \item{$expr}{a data frame with 20214 genes (rows) amd 90 cells (columns)}
+#'   \item{$meta$Cluster}{fct cell type}
 #' }
 #' @source \url{https://www.nature.com/articles/nsmb.2660}
 "yan_2013"
@@ -15,13 +16,12 @@
 #'
 #' Zeisel 2015: Mouse Brain, 7 cell types, 3005 cells
 #'
-#' @format A list with expr and labels:
-#' $expr: a data frame with 19972 genes (rows) amd 3005 cells (columns)
-#' $meta:
+#' @format A list with expression matrix and metadata:
 #' #'\describe{
-#'   \item{Label}{fct cell type}
-#'   \item{Sex}{fct random generated sex info}
-#'   \item{Sample}{fct random generated sample info}
+#'   \item{$expr}{a data frame with 19972 genes (rows) amd 3005 cells (columns)}
+#'   \item{$meta$Label}{fct cell type}
+#'   \item{$meta$Sex}{fct random generated sex info}
+#'   \item{$meta$Sample}{fct random generated sample info}
 #' }
 #' @source \url{https://science.sciencemag.org/content/347/6226/1138.abstract}
 "zeisel_2015"
