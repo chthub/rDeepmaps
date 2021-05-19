c8e5471fd8c9661f84f2335b4ea5ebbb6aba1ac6
Cankun Wang <flykun0620@gmail.com>
2021-05-18 20:56:12 -0400

    Update socket url

--- R/socket_service.R
+++ R/socket_service.R
@@ -25,9 +25,9 @@ disconnect_socketio <- function() {
 #' @export
 #'
 send_progress <- function(message = "message from R") {
-  e2$sio$connect("http://127.0.0.1:9005")
+  #e2$sio$connect("http://127.0.0.1:9005")
   # e2$sio$connect("https://bmbls.bmi.osumc.edu/deepmaps/api/socket.io")
-  # e2$sio$connect("https://bmbls.bmi.osumc.edu")
+  e2$sio$connect("https://bmbls.bmi.osumc.edu")
   e2$sio$emit("jobProgress", list(event = "jobProgress", data = message))
   # Don't know why the last emit will be ignored, could be async issue,
   # fixed this by emit a empty message
@@ -36,9 +36,10 @@ send_progress <- function(message = "message from R") {
   e2$sio$disconnect()
 }
 
-# i=1
-# init_socketio()
-# for(i in 1:10) {
-#  Sys.sleep(0.5)
-#  send_progress(i)
-# }
+ i=1
+ init_socketio()
+ for(i in 1:10) {
+  Sys.sleep(0.5)
+  send_progress(i)
+ }
+
