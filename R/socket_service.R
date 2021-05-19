859d919df0a2f74bdb25dc58e0ca028f3530f4dd
Cankun Wang <flykun0620@gmail.com>
2021-05-18 21:15:01 -0400

    Update socket io url

--- R/socket_service.R
+++ R/socket_service.R
@@ -28,20 +28,20 @@ send_progress <- function(message = "message from R") {
   io <- reticulate::import("socketio")
   e2$sio <- io$Client(logger = F, engineio_logger = F)
   #e2$sio$connect("http://127.0.0.1:9005")
-  # e2$sio$connect("https://bmbls.bmi.osumc.edu/deepmaps/api/socket.io")
+  #e2$sio$connect("https://bmbls.bmi.osumc.edu/deepmaps/api/socket.io")
   e2$sio$connect("https://bmbls.bmi.osumc.edu")
-  e2$sio$emit("jobProgress", list(event = "jobProgress", data = message))
+  e2$sio$emit('jobProgress', list(event = "jobProgress", data = message))
   # Don't know why the last emit will be ignored, could be async issue,
   # fixed this by emit a empty message
   Sys.sleep(0.1)
-  e2$sio$emit("empty", "1")
-  e2$sio$disconnect()
+  e2$sio$emit('empty', "1")
+  #e2$sio$disconnect()
 }
 
-# i=1
-# init_socketio()
-# for(i in 1:10) {
-#  Sys.sleep(0.5)
-#  send_progress(i)
-# }
+
+ #i=1
+ #for(i in 1:10) {
+ # Sys.sleep(0.5)
+ # send_progress(i)
+ #}
 
